fac1fdc124cf12e16bd6119ab9dbf2ab67001eff
Muhammed Ameen <muhammedameen08@gmail.com>
2019-11-19 15:48:35 +0530

    Create assign_1_Client.c

---
+++ assign_1_Client.c
@@ -0,0 +1,52 @@
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
+#include<unistd.h>
+#include<arpa/inet.h>
+#define PORT 1234
+
+int main(int argc, char const *argv[]) 
+{ 
+	struct sockaddr_in address; 
+	int sock = 0, valread; 
+	struct sockaddr_in serv_addr; 
+	char message_server[1000]; 
+	char buffer[1024] = {0}; 
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+	{ 
+		printf("\n Socket creation error \n"); 
+		return -1; 
+	} 
+
+	memset(&serv_addr, '0', sizeof(serv_addr)); 
+
+	serv_addr.sin_family = AF_INET; 
+	serv_addr.sin_port = htons(PORT); 
+	
+	// Convert IPv4 and IPv6 addresses from text to binary form 
+	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+	{ 
+		printf("\nInvalid address/ Address not supported \n"); 
+		return -1; 
+	} 
+
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+	{ 
+		printf("\nConnection Failed \n"); 
+		return -1; 
+	} 
+	int check=1;
+	while(check){
+		printf("Enter the message :");
+		scanf("%[^\n]%*c", message_server);
+		send(sock , message_server, strlen(message_server) , 0 ); 
+		valread = read( sock , buffer, 1024); 
+		check=0;
+		printf("%s\n",buffer);
+		if(strcmp(buffer,"Ok")==0) 
+			check=1;
+	}
+	return 0; 
+} 
